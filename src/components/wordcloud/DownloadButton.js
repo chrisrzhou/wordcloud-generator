7d970c8fcf2990a5aaa4240f9e27170ba2e691ac
Chris Zhou <christopher.r.zhou@gmail.com>
2018-03-02 00:04:42 -0800

    add download feature

--- src/components/wordcloud/DownloadButton.js
+++ src/components/wordcloud/DownloadButton.js
@@ -1,8 +1,14 @@
 import Button from 'antd/lib/button';
+import * as d3Selection from 'd3-selection';
 import React from 'react';
+import {saveSvgAsPng} from 'save-svg-as-png';
 
 const DownloadButton = () => (
-  <Button size="small" onClick={() => alert('download png')}>
+  <Button
+    size="small"
+    onClick={() => {
+      saveSvgAsPng(d3Selection.select('svg').node(), 'wordcloud.png');
+    }}>
     Download
   </Button>
 );
